5b69a54e2b7fac55a0a2558142517b280eddbc50
lourduradjou <rajlourdu15@gmail.com>
2024-11-18 17:48:39 +0530

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/connectDb')
 
 const app = express()
+dotenv.config({ path: '../.env' })
+
+connectDB()
 
 app.get('/', (req, res) => {
 	res.send('Hello India!')
 })
 
-app.listen(5000, console.log('server is running'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`server is running on ${PORT}`))
